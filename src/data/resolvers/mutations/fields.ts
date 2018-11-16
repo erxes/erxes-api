3c9c2168731250d98741e5c7e92bdd6cfbcac9d0
batamar <batamar.b@nmtec.co>
2018-11-16 11:03:49 +0800

    Converted field mutations

--- src/data/resolvers/mutations/fields.ts
+++ src/data/resolvers/mutations/fields.ts
@@ -1,6 +1,5 @@
-import { Fields, FieldsGroups } from '../../../db/models';
+import { IContext } from '../../../connectionResolver';
 import { IField, IFieldGroup } from '../../../db/models/definitions/fields';
-import { IUserDocument } from '../../../db/models/definitions/users';
 import { IOrderInput } from '../../../db/models/Fields';
 import { moduleRequireLogin } from '../../permissions';
 
@@ -16,28 +15,28 @@ const fieldMutations = {
   /**
    * Adds field object
    */
-  fieldsAdd(_root, args: IField, { user }: { user: IUserDocument }) {
+  fieldsAdd(_root, args: IField, { user, models: { Fields } }: IContext) {
     return Fields.createField({ ...args, lastUpdatedUserId: user._id });
   },
 
   /**
    * Updates field object
    */
-  fieldsEdit(_root, { _id, ...doc }: IFieldsEdit, { user }: { user: IUserDocument }) {
+  fieldsEdit(_root, { _id, ...doc }: IFieldsEdit, { user, models: { Fields } }: IContext) {
     return Fields.updateField(_id, { ...doc, lastUpdatedUserId: user._id });
   },
 
   /**
    * Remove a channel
    */
-  fieldsRemove(_root, { _id }: { _id: string }) {
+  fieldsRemove(_root, { _id }: { _id: string }, { models: { Fields } }: IContext) {
     return Fields.removeField(_id);
   },
 
   /**
    * Update field orders
    */
-  fieldsUpdateOrder(_root, { orders }: { orders: IOrderInput[] }) {
+  fieldsUpdateOrder(_root, { orders }: { orders: IOrderInput[] }, { models: { Fields } }: IContext) {
     return Fields.updateOrder(orders);
   },
 
@@ -47,7 +46,7 @@ const fieldMutations = {
   fieldsUpdateVisible(
     _root,
     { _id, isVisible }: { _id: string; isVisible: boolean },
-    { user }: { user: IUserDocument },
+    { user, models: { Fields } }: IContext,
   ) {
     return Fields.updateFieldsVisible(_id, isVisible, user._id);
   },
@@ -57,14 +56,14 @@ const fieldsGroupsMutations = {
   /**
    * Create a new group for fields
    */
-  fieldsGroupsAdd(_root, doc: IFieldGroup, { user }: { user: IUserDocument }) {
+  fieldsGroupsAdd(_root, doc: IFieldGroup, { user, models: { FieldsGroups } }: IContext) {
     return FieldsGroups.createGroup({ ...doc, lastUpdatedUserId: user._id });
   },
 
   /**
    * Update group for fields
    */
-  fieldsGroupsEdit(_root, { _id, ...doc }: IFieldsGroupsEdit, { user }: { user: IUserDocument }) {
+  fieldsGroupsEdit(_root, { _id, ...doc }: IFieldsGroupsEdit, { user, models: { FieldsGroups } }: IContext) {
     return FieldsGroups.updateGroup(_id, {
       ...doc,
       lastUpdatedUserId: user._id,
@@ -74,7 +73,7 @@ const fieldsGroupsMutations = {
   /**
    * Remove group
    */
-  fieldsGroupsRemove(_root, { _id }: { _id: string }) {
+  fieldsGroupsRemove(_root, { _id }: { _id: string }, { models: { FieldsGroups } }: IContext) {
     return FieldsGroups.removeGroup(_id);
   },
 
@@ -84,7 +83,7 @@ const fieldsGroupsMutations = {
   fieldsGroupsUpdateVisible(
     _root,
     { _id, isVisible }: { _id: string; isVisible: boolean },
-    { user }: { user: IUserDocument },
+    { user, models: { FieldsGroups } }: IContext,
   ) {
     return FieldsGroups.updateGroupVisible(_id, isVisible, user._id);
   },
