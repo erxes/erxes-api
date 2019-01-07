f6c4d159228979157144550e2d1ab54c8868e729
Erdenezul Batmunkh <erdenezul.b@gmail.com>
2019-01-07 20:37:48 +0800

    Specify Promise type

--- src/data/resolvers/queries/aggregationUtils.ts
+++ src/data/resolvers/queries/aggregationUtils.ts
@@ -3,7 +3,15 @@
  * @param fieldName
  * @param timeFormat
  */
-export const getDateFieldAsStr = async ({ fieldName = '$createdAt', timeFormat = '%Y-%m-%d' }): Promise<any> => {
+export const getDateFieldAsStr = async ({
+  fieldName = '$createdAt',
+  timeFormat = '%Y-%m-%d',
+}): Promise<{
+  $dateToString: {
+    format: string;
+    date: string;
+  };
+}> => {
   return {
     $dateToString: {
       format: timeFormat,
