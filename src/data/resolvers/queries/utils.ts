2a67df2c251794ee0a82feebf330bd0de2e97f84
Munkh-orgil <munkhorgil@live.com>
2019-05-21 14:42:50 +0800

    Add ids to product query

--- src/data/resolvers/queries/utils.ts
+++ src/data/resolvers/queries/utils.ts
@@ -2,7 +2,7 @@ export const paginate = (collection, params: { page?: number; perPage?: number }
   const { page = 0, perPage = 0 } = params || {};
 
   const _page = Number(page || '1');
-  const _limit = Number(perPage || '5');
+  const _limit = Number(perPage || '20');
 
   return collection.limit(_limit).skip((_page - 1) * _limit);
 };
