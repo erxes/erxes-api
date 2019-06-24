1755eb62d93b3d3975e6babe34eb91a227993af4
batamar <batamar.b@nmtec.co>
2019-06-24 21:24:36 +0800

    Changed some debuggers format

--- src/debuggers.ts
+++ src/debuggers.ts
@@ -2,8 +2,8 @@ import * as debug from 'debug';
 
 export const debugExternalApi = debug('erxes-api:external-api-fetcher');
 export const debugInit = debug('erxes-api:init');
-export const debugCrons = debug('erxes-crons');
-export const debugWorkers = debug('erxes-workers');
+export const debugCrons = debug('erxes-crons:');
+export const debugWorkers = debug('erxes-workers:');
 export const debugDb = debug('erxes-api:db');
 export const debugImport = debug('erxes-api:import');
 export const debugBase = debug('erxes-api:base');
