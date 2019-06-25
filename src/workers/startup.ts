97cb5c5b798bb741c0382bd3c7f910885fb4f2df
batamar <batamar.b@nmtec.co>
2019-06-25 18:06:51 +0800

    Fixed xlsxTemplatOutputs folder location

--- src/workers/startup.ts
+++ src/workers/startup.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 export const init = async () => {
   const makeDirs = () => {
-    const dir = `${__dirname}/private/xlsTemplateOutputs`;
+    const dir = `${__dirname}/../private/xlsTemplateOutputs`;
 
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
