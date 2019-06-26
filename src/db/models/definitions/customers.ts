f64c5d2502a116c391802518abed82a140f4f3df
Erdenezul Batmunkh <erdenezul@shine.mn>
2019-06-26 09:26:50 +0200

    Add optional

--- src/db/models/definitions/customers.ts
+++ src/db/models/definitions/customers.ts
@@ -164,7 +164,7 @@ export const customerSchema = new Schema({
 
   primaryPhone: field({ type: String, label: 'Primary Phone', optional: true }),
   phones: field({ type: [String], optional: true }),
-  profileScore: field({ type: Number, index: true }),
+  profileScore: field({ type: Number, index: true, optional: true }),
 
   ownerId: field({ type: String, optional: true }),
   position: field({ type: String, optional: true, label: 'Position' }),
