51ec070ff3dcceef039f7db15e7ccc3e6b4e4c56
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-07-04 18:44:15 +0800

    enable log recording in product mutations

--- src/data/resolvers/mutations/products.ts
+++ src/data/resolvers/mutations/products.ts
@@ -1,6 +1,9 @@
 import { Products } from '../../../db/models';
 import { IProduct } from '../../../db/models/definitions/deals';
+import { IUserDocument } from '../../../db/models/definitions/users';
+import { LOG_ACTIONS } from '../../constants';
 import { moduleCheckPermission } from '../../permissions/wrappers';
+import { putLog } from '../../utils';
 
 interface IProductsEdit extends IProduct {
   _id: string;
@@ -8,24 +11,79 @@ interface IProductsEdit extends IProduct {
 
 const productMutations = {
   /**
-   * Create new product
+   * Creates a new product
+   * @param {Object} doc Product document
    */
-  productsAdd(_root, doc: IProduct) {
-    return Products.createProduct(doc);
+  async productsAdd(_root, doc: IProduct, { user }: { user: IUserDocument }) {
+    const product = await Products.createProduct(doc);
+
+    if (product) {
+      await putLog({
+        body: {
+          createdBy: user._id,
+          type: 'product',
+          action: LOG_ACTIONS.CREATE,
+          newData: JSON.stringify(doc),
+          objectId: product._id,
+          unicode: user.username || user.email || user._id,
+          description: `${product.name} has been created`,
+        },
+      });
+    }
+
+    return product;
   },
 
   /**
-   * Edit product
+   * Edits a product
+   * @param {string} param2._id Product id
+   * @param {Object} param2.doc Product info
    */
-  productsEdit(_root, { _id, ...doc }: IProductsEdit) {
-    return Products.updateProduct(_id, doc);
+  async productsEdit(_root, { _id, ...doc }: IProductsEdit, { user }: { user: IUserDocument }) {
+    const found = await Products.findOne({ _id });
+    const updated = await Products.updateProduct(_id, doc);
+
+    if (found && updated) {
+      await putLog({
+        body: {
+          createdBy: user._id,
+          type: 'product',
+          action: LOG_ACTIONS.UPDATE,
+          objectId: _id,
+          oldData: JSON.stringify(found),
+          newData: JSON.stringify(doc),
+          unicode: user.username || user.email || user._id,
+          description: `${found.name} has been edited`,
+        },
+      });
+    }
+
+    return updated;
   },
 
   /**
-   * Remove product
+   * Removes a product
+   * @param {string} param1._id Product id
    */
-  productsRemove(_root, { _id }: { _id: string }) {
-    return Products.removeProduct(_id);
+  async productsRemove(_root, { _id }: { _id: string }, { user }: { user: IUserDocument }) {
+    const found = await Products.findOne({ _id });
+    const removed = await Products.removeProduct(_id);
+
+    if (found) {
+      await putLog({
+        body: {
+          createdBy: user._id,
+          type: 'product',
+          action: LOG_ACTIONS.DELETE,
+          oldData: JSON.stringify(found),
+          objectId: _id,
+          unicode: user.username || user.email || user._id,
+          description: `${found.name} has been removed`,
+        },
+      });
+    }
+
+    return removed;
   },
 };
 
