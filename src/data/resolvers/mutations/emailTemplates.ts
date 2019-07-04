1f1b37517322f95a2f5196dac4d7d92c9c48c9fd
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-07-04 15:16:50 +0800

    enable log recording in email template mutations

--- src/data/resolvers/mutations/emailTemplates.ts
+++ src/data/resolvers/mutations/emailTemplates.ts
@@ -1,6 +1,9 @@
 import { EmailTemplates } from '../../../db/models';
 import { IEmailTemplate } from '../../../db/models/definitions/emailTemplates';
+import { IUserDocument } from '../../../db/models/definitions/users';
+import { LOG_ACTIONS } from '../../constants';
 import { moduleCheckPermission } from '../../permissions/wrappers';
+import { putLog } from '../../utils';
 
 interface IEmailTemplatesEdit extends IEmailTemplate {
   _id: string;
@@ -10,22 +13,73 @@ const emailTemplateMutations = {
   /**
    * Create new email template
    */
-  emailTemplatesAdd(_root, doc: IEmailTemplate) {
-    return EmailTemplates.create(doc);
+  async emailTemplatesAdd(_root, doc: IEmailTemplate, { user }: { user: IUserDocument }) {
+    const template = await EmailTemplates.create(doc);
+
+    if (template) {
+      await putLog({
+        body: {
+          createdBy: user._id,
+          type: 'emailTemplate',
+          action: LOG_ACTIONS.CREATE,
+          newData: JSON.stringify(doc),
+          objectId: template._id,
+          unicode: user.username || user.email || user._id,
+          description: `${template.name} has been created`,
+        },
+      });
+    }
+
+    return template;
   },
 
   /**
    * Update email template
    */
-  emailTemplatesEdit(_root, { _id, ...fields }: IEmailTemplatesEdit) {
-    return EmailTemplates.updateEmailTemplate(_id, fields);
+  async emailTemplatesEdit(_root, { _id, ...fields }: IEmailTemplatesEdit, { user }: { user: IUserDocument }) {
+    const found = await EmailTemplates.findOne({ _id });
+    const updated = await EmailTemplates.updateEmailTemplate(_id, fields);
+
+    if (found && updated) {
+      await putLog({
+        body: {
+          createdBy: user._id,
+          type: 'emailTemplate',
+          action: LOG_ACTIONS.UPDATE,
+          oldData: JSON.stringify(found),
+          newData: JSON.stringify(fields),
+          objectId: _id,
+          unicode: user.username || user.email || user._id,
+          description: `${found.name} has been edited`,
+        },
+      });
+    }
+
+    return updated;
   },
 
   /**
    * Delete email template
    */
-  emailTemplatesRemove(_root, { _id }: { _id: string }) {
-    return EmailTemplates.removeEmailTemplate(_id);
+  async emailTemplatesRemove(_root, { _id }: { _id: string }, { user }: { user: IUserDocument }) {
+    const template = await EmailTemplates.findOne({ _id });
+    const removed = await EmailTemplates.removeEmailTemplate(_id);
+
+    if (template) {
+      await putLog({
+        body: {
+          createdBy: user._id,
+          type: 'emailTemplate',
+          action: LOG_ACTIONS.DELETE,
+          oldData: JSON.stringify(template),
+          objectId: _id,
+          unicode: user.username || user.email || user._id,
+          description: `${template.name} has been removed`,
+        },
+      });
+    }
+
+    return removed;
   },
 };
 
