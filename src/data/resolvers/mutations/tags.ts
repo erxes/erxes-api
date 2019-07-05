8a42c4abdd0fff319d72bf9659ae9f4bdb0ed160
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-07-05 18:25:33 +0800

    enable log recording in tag mutations

--- src/data/resolvers/mutations/tags.ts
+++ src/data/resolvers/mutations/tags.ts
@@ -1,6 +1,9 @@
 import { Tags } from '../../../db/models';
 import { ITag } from '../../../db/models/definitions/tags';
+import { IUserDocument } from '../../../db/models/definitions/users';
+import { LOG_ACTIONS } from '../../constants';
 import { checkPermission, requireLogin } from '../../permissions/wrappers';
+import { putLog } from '../../utils';
 import { publishConversationsChanged } from './conversations';
 
 interface ITagsEdit extends ITag {
@@ -11,22 +14,68 @@ const tagMutations = {
   /**
    * Create new tag
    */
-  tagsAdd(_root, doc: ITag) {
-    return Tags.createTag(doc);
+  async tagsAdd(_root, doc: ITag, { user }: { user: IUserDocument }) {
+    const tag = await Tags.createTag(doc);
+
+    if (tag) {
+      await putLog({
+        createdBy: user._id,
+        type: 'tag',
+        action: LOG_ACTIONS.CREATE,
+        objectId: tag._id,
+        newData: JSON.stringify(tag),
+        unicode: user.username || user.email || user._id,
+        description: `${tag.name} has been created`,
+      });
+    }
+
+    return tag;
   },
 
   /**
    * Edit tag
    */
-  tagsEdit(_root, { _id, ...doc }: ITagsEdit) {
-    return Tags.updateTag(_id, doc);
+  async tagsEdit(_root, { _id, ...doc }: ITagsEdit, { user }: { user: IUserDocument }) {
+    const tag = await Tags.findOne({ _id });
+    const updated = await Tags.updateTag(_id, doc);
+
+    if (tag && updated) {
+      await putLog({
+        createdBy: user._id,
+        type: 'tag',
+        action: LOG_ACTIONS.UPDATE,
+        objectId: tag._id,
+        oldData: JSON.stringify(tag),
+        newData: JSON.stringify(doc),
+        unicode: user.username || user.email || user._id,
+        description: `${tag.name} has been edited`,
+      });
+    }
+
+    return updated;
   },
 
   /**
    * Remove tag
    */
-  tagsRemove(_root, { ids }: { ids: string[] }) {
-    return Tags.removeTag(ids);
+  async tagsRemove(_root, { ids }: { ids: string[] }, { user }: { user: IUserDocument }) {
+    const tags = await Tags.find({ _id: { $in: ids } });
+    const removed = await Tags.removeTag(ids);
+
+    for (const tag of tags) {
+      await putLog({
+        createdBy: user._id,
+        type: 'tag',
+        action: LOG_ACTIONS.DELETE,
+        oldData: JSON.stringify(tag),
+        newData: '',
+        objectId: tag._id,
+        unicode: user.username || user.email || user._id,
+        description: `${tag.name} has been removed`,
+      });
+    }
+
+    return removed;
   },
 
   /**
