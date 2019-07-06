b5b016e11451b23b13c85355d2e9e99338385cc7
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-07-06 23:03:48 +0800

    enable log recording in kb topic mutations

--- src/data/resolvers/mutations/knowledgeBase.ts
+++ src/data/resolvers/mutations/knowledgeBase.ts
@@ -3,28 +3,75 @@ import { KnowledgeBaseArticles, KnowledgeBaseCategories, KnowledgeBaseTopics } f
 import { ITopic } from '../../../db/models/definitions/knowledgebase';
 import { IUserDocument } from '../../../db/models/definitions/users';
 import { IArticleCreate, ICategoryCreate } from '../../../db/models/KnowledgeBase';
+import { LOG_ACTIONS } from '../../constants';
 import { moduleCheckPermission } from '../../permissions/wrappers';
+import { putLog } from '../../utils';
 
 const knowledgeBaseMutations = {
   /**
    * Create topic document
    */
-  knowledgeBaseTopicsAdd(_root, { doc }: { doc: ITopic }, { user }: { user: IUserDocument }) {
-    return KnowledgeBaseTopics.createDoc(doc, user._id);
+  async knowledgeBaseTopicsAdd(_root, { doc }: { doc: ITopic }, { user }: { user: IUserDocument }) {
+    const topic = await KnowledgeBaseTopics.createDoc(doc, user._id);
+
+    if (topic) {
+      await putLog({
+        createdBy: user._id,
+        type: 'knowledgeBaseTopic',
+        action: LOG_ACTIONS.CREATE,
+        newData: JSON.stringify(doc),
+        objectId: topic._id,
+        unicode: user.username || user.email || user._id,
+        description: `${topic.title} has been created`,
+      });
+    }
+
+    return topic;
   },
 
   /**
    * Update topic document
    */
-  knowledgeBaseTopicsEdit(_root, { _id, doc }: { _id: string; doc: ITopic }, { user }: { user: IUserDocument }) {
-    return KnowledgeBaseTopics.updateDoc(_id, doc, user._id);
+  async knowledgeBaseTopicsEdit(_root, { _id, doc }: { _id: string; doc: ITopic }, { user }: { user: IUserDocument }) {
+    const topic = await KnowledgeBaseTopics.findOne({ _id });
+    const updated = await KnowledgeBaseTopics.updateDoc(_id, doc, user._id);
+
+    if (topic && updated) {
+      await putLog({
+        createdBy: user._id,
+        type: 'knowledgeBaseTopic',
+        action: LOG_ACTIONS.UPDATE,
+        oldData: JSON.stringify(topic),
+        newData: JSON.stringify(doc),
+        objectId: _id,
+        unicode: user.username || user.email || user._id,
+        description: `${topic.title} has been edited`,
+      });
+    }
+
+    return updated;
   },
 
   /**
    * Remove topic document
    */
-  knowledgeBaseTopicsRemove(_root, { _id }: { _id: string }) {
-    return KnowledgeBaseTopics.removeDoc(_id);
+  async knowledgeBaseTopicsRemove(_root, { _id }: { _id: string }, { user }: { user: IUserDocument }) {
+    const topic = await KnowledgeBaseTopics.findOne({ _id });
+    const removed = await KnowledgeBaseTopics.removeDoc(_id);
+
+    if (topic) {
+      await putLog({
+        createdBy: user._id,
+        type: 'knowledgeBaseTopic',
+        action: LOG_ACTIONS.DELETE,
+        oldData: JSON.stringify(topic),
+        objectId: _id,
+        unicode: user.username || user.email || user._id,
+        description: `${topic.title} has been removed`,
+      });
+    }
+
+    return removed;
   },
 
   /**
