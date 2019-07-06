6050072f39c3813f5f17b3720c8a7b876f876e8a
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-07-06 23:02:45 +0800

    enable log recording in internal note mutations

--- src/data/resolvers/mutations/internalNotes.ts
+++ src/data/resolvers/mutations/internalNotes.ts
@@ -1,7 +1,9 @@
 import { InternalNotes } from '../../../db/models';
 import { IInternalNote } from '../../../db/models/definitions/internalNotes';
 import { IUserDocument } from '../../../db/models/definitions/users';
+import { LOG_ACTIONS } from '../../constants';
 import { moduleRequireLogin } from '../../permissions/wrappers';
+import { putLog } from '../../utils';
 
 interface IInternalNotesEdit extends IInternalNote {
   _id: string;
@@ -14,21 +16,64 @@ const internalNoteMutations = {
   async internalNotesAdd(_root, args: IInternalNote, { user }: { user: IUserDocument }) {
     const internalNote = await InternalNotes.createInternalNote(args, user);
 
+    if (internalNote) {
+      await putLog({
+        createdBy: user._id,
+        type: 'internalNote',
+        action: LOG_ACTIONS.CREATE,
+        newData: JSON.stringify(args),
+        objectId: internalNote._id,
+        unicode: user.username || user.email || user._id,
+        description: `${internalNote.contentType} has been created`,
+      });
+    }
+
     return internalNote;
   },
 
   /**
    * Updates internalNote object
    */
-  internalNotesEdit(_root, { _id, ...doc }: IInternalNotesEdit) {
-    return InternalNotes.updateInternalNote(_id, doc);
+  async internalNotesEdit(_root, { _id, ...doc }: IInternalNotesEdit, { user }: { user: IUserDocument }) {
+    const internalNote = await InternalNotes.findOne({ _id });
+    const updated = await InternalNotes.updateInternalNote(_id, doc);
+
+    if (internalNote && updated) {
+      await putLog({
+        createdBy: user._id,
+        type: 'internalNote',
+        action: LOG_ACTIONS.UPDATE,
+        objectId: _id,
+        oldData: JSON.stringify(internalNote),
+        newData: JSON.stringify(doc),
+        unicode: user.username || user.email || user._id,
+        description: `${internalNote.contentType} written at ${internalNote.createdDate} has been edited`,
+      });
+    }
+
+    return updated;
   },
 
   /**
    * Remove a channel
    */
-  internalNotesRemove(_root, { _id }: { _id: string }) {
-    return InternalNotes.removeInternalNote(_id);
+  async internalNotesRemove(_root, { _id }: { _id: string }, { user }: { user: IUserDocument }) {
+    const internalNote = await InternalNotes.findOne({ _id });
+    const removed = await InternalNotes.removeInternalNote(_id);
+
+    if (internalNote) {
+      await putLog({
+        createdBy: user._id,
+        type: 'internalNote',
+        action: LOG_ACTIONS.DELETE,
+        oldData: JSON.stringify(internalNote),
+        objectId: _id,
+        unicode: user.username || user.email || user._id,
+        description: `${internalNote.contentType} written at ${internalNote.createdDate} has been removed`,
+      });
+    }
+
+    return removed;
   },
 };
 
