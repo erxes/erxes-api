b1e9794632b5cdb3e0501c2a8baa1d3dddbbea44
Enkhtuvshin <enktuvshin.n@nmma.co>
2019-07-07 23:28:42 +0800

    Mention on deal note

--- src/data/resolvers/mutations/internalNotes.ts
+++ src/data/resolvers/mutations/internalNotes.ts
@@ -1,7 +1,8 @@
-import { InternalNotes } from '../../../db/models';
+import { Deals, InternalNotes, Pipelines, Stages } from '../../../db/models';
 import { IInternalNote } from '../../../db/models/definitions/internalNotes';
 import { IUserDocument } from '../../../db/models/definitions/users';
 import { moduleRequireLogin } from '../../permissions/wrappers';
+import utils from '../../utils';
 
 interface IInternalNotesEdit extends IInternalNote {
   _id: string;
@@ -12,6 +13,42 @@ const internalNoteMutations = {
    * Adds internalNote object and also adds an activity log
    */
   async internalNotesAdd(_root, args: IInternalNote, { user }: { user: IUserDocument }) {
+    switch (args.contentType) {
+      case 'deal': {
+        const deal = await Deals.findOne({ _id: args.contentTypeId });
+
+        if (!deal) {
+          throw new Error('Deal not found');
+        }
+
+        const stage = await Stages.findOne({ _id: deal.stageId });
+
+        if (!stage) {
+          throw new Error('Stage not found');
+        }
+
+        const pipeline = await Pipelines.findOne({ _id: stage.pipelineId });
+
+        if (!pipeline) {
+          throw new Error('Pipeline not found');
+        }
+
+        const title = `${user.details ? user.details.fullName : 'Someone'} mentioned you in deal note`;
+
+        utils.sendNotification({
+          createdUser: user._id,
+          notifType: 'channelMembersChange',
+          title,
+          content: title,
+          link: `/deal/board?id=${pipeline.boardId}&pipelineId=${pipeline._id}`,
+          receivers: args.mentionedUserIds || [],
+        });
+      }
+
+      default:
+        break;
+    }
+
     const internalNote = await InternalNotes.createInternalNote(args, user);
 
     return internalNote;
