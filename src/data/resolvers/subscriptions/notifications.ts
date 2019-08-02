c7162eac9d619b1f29e8239b9c46642715ccef82
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2019-08-02 15:29:18 +0800

    Added userId variable for notification subscription

--- src/data/resolvers/subscriptions/notifications.ts
+++ src/data/resolvers/subscriptions/notifications.ts
@@ -1,3 +1,4 @@
+import { withFilter } from 'apollo-server-express';
 import { graphqlPubsub } from '../../../pubsub';
 
 export default {
@@ -5,6 +6,11 @@ export default {
    * Listen for notification
    */
   notificationInserted: {
-    subscribe: () => graphqlPubsub.asyncIterator('notificationInserted'),
+    subscribe: withFilter(
+      () => graphqlPubsub.asyncIterator('notificationInserted'),
+      (payload, variables) => {
+        return payload.notificationInserted.userId === variables.userId;
+      },
+    ),
   },
 };
