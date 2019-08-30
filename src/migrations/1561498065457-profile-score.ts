7e9039e4b98b32014ac55392c0123551150602b8
munkhjin <munkhjin0223@gmail.com>
2019-08-30 15:35:43 +0800

    update

--- src/migrations/1561498065457-profile-score.ts
+++ src/migrations/1561498065457-profile-score.ts
@@ -1,5 +1,5 @@
 import { connect } from '../db/connection';
-// import { Customers } from '../db/models';
+import { Customers } from '../db/models';
 
 /**
  * Updating profile scores on customers
@@ -8,20 +8,20 @@ import { connect } from '../db/connection';
 module.exports.up = async () => {
   await connect();
 
-  // await Customers.ensureIndexes();
+  await Customers.ensureIndexes();
 
-  // let customerCount = await Customers.countDocuments({ profileScore: { $exists: false } });
+  let customerCount = await Customers.countDocuments({ profileScore: { $exists: false } });
 
-  // while (customerCount > 0) {
-  //   const bulks = [];
+  while (customerCount > 0) {
+    const bulks = [];
 
-  //   for (const customer of await Customers.find({ profileScore: { $exists: false } }).limit(200)) {
-  //     bulks.push(await Customers.updateProfileScore(customer._id, false));
-  //     customerCount -= 1;
-  //   }
+    for (const customer of await Customers.find({ profileScore: { $exists: false } }).limit(200)) {
+      bulks.push(await Customers.updateProfileScore(customer._id, false));
+      customerCount -= 1;
+    }
 
-  //   await Customers.bulkWrite(bulks);
-  // }
+    await Customers.bulkWrite(bulks);
+  }
 
   return Promise.resolve('ok');
 };
