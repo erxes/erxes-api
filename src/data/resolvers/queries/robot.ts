838525d5f8c8808497f9bb8a1bdabd295c97c72f
BatAmar Battulga <batamar.b@nmtec.co>
2019-09-28 15:08:58 +0800

    refactor feature

--- src/data/resolvers/queries/robot.ts
+++ src/data/resolvers/queries/robot.ts
@@ -1,50 +1,55 @@
 import { RobotEntries } from '../../../db/models';
 import { OnboardingHistories } from '../../../db/models/Robot';
+import { moduleObjects } from '../../permissions/actions/permission';
+import { getUserAllowedActions, IModuleMap } from '../../permissions/utils';
 import { moduleRequireLogin } from '../../permissions/wrappers';
 import { IContext } from '../../types';
 
-interface IAction {
-  name: string;
-  url: string;
-}
+const featureActions: { [key: string]: string[] } = {
+  inbox: ['manageBrands', 'manageChannels', 'integrationCreate'],
+  customers: [],
+  deals: ['dealBoardsAdd', 'dealPipelinesAdd', 'dealStagesAdd'],
+  tasks: ['taskBoardsAdd', 'taskPipelinesAdd', 'taskStagesAdd'],
+  tickets: ['ticketBoardsAdd', 'ticketPipelinesAdd', 'ticketStagesAdd'],
+  growthHacks: ['growthHackBoardsAdd', 'growthHackPipelinesAdd', 'growthHackStagesAdd', 'managePipelineTemplates'],
+  engages: ['manageEmailTemplate', 'manageTags'],
+  leads: ['manageBrands'],
+  knowledgeBase: ['manageKnowledgeBase'],
+  tags: ['manageTags'],
+  insights: [],
+  importHistories: [],
+  segments: [],
+  properties: [],
+  permissions: [],
+  integrations: ['manageBrands', 'manageChannels'],
+};
 
-interface IFeature {
-  name: string;
-  text: string;
-  description?: string;
-  videoUrl?: string;
-  actions?: IAction[];
-}
+const checkShowModule = (actionsMap, moduleName: string): { showModule: boolean; showActions: boolean } => {
+  const actions = featureActions[moduleName];
+  const module: IModuleMap = moduleObjects[moduleName];
 
-const FEATURES: IFeature[] = [
-  {
-    name: 'inbox',
-    text: 'Inbox',
-    description: 'Inbox description',
-    videoUrl: '',
-    actions: [
-      { name: 'brandCreate', url: '/settings/brands' },
-      { name: 'channelCreate', url: '/settings/channels' },
-      { name: 'integrationCreate', url: '/settings/integrations' },
-    ],
-  },
+  let showModule = false;
+  let showActions = true;
 
-  { name: 'contacts', text: 'Contact' },
-  { name: 'deals', text: 'Deals' },
-  { name: 'tasks', text: 'Tasks' },
-  { name: 'tickets', text: 'Tickets' },
-  { name: 'growthHacks', text: 'Growth Hacks' },
-  { name: 'engages', text: 'Engages' },
-  { name: 'leads', text: 'Leads' },
-  { name: 'knowledgebase', text: 'Knowledgebase' },
-  { name: 'insights', text: 'Insights' },
-  { name: 'imports', text: 'Imports' },
-  { name: 'tags', text: 'Tags' },
-  { name: 'segments', text: 'Segments' },
-  { name: 'propertes', text: 'Properties' },
-  { name: 'permissions', text: 'Permissions' },
-  { name: 'integrations', text: 'Integrations' },
-];
+  for (const action of module.actions || []) {
+    if (actionsMap.includes(action.name || '')) {
+      showModule = true;
+      break;
+    }
+  }
+
+  for (const action of actions) {
+    if (actionsMap.includes(action)) {
+      showActions = false;
+      break;
+    }
+  }
+
+  return {
+    showModule,
+    showActions,
+  };
+};
 
 const robotQueries = {
   robotEntries(_root) {
@@ -56,15 +61,41 @@ const robotQueries = {
   },
 
   async onboardingGetAvailableFeatures(_root, _args, { user }: IContext) {
-    const results: Array<{ feature: IFeature; isComplete: boolean }> = [];
+    const results: Array<{ feature: string; isComplete: boolean; actions?: string[]; showActions?: boolean }> = [];
+    const actionsMap = await getUserAllowedActions(user);
 
-    for (const feature of FEATURES) {
-      const selector = { userId: user._id, completedActions: { $all: (feature.actions || []).map(a => a.name) } };
+    for (const feature of Object.keys(featureActions)) {
+      if (['leads', 'properties'].includes(feature)) {
+        if (actionsMap.includes('integrationsCreateLeadIntegration')) {
+          results.push({
+            feature: 'leads',
+            isComplete: true,
+          });
+        }
 
-      results.push({
-        feature,
-        isComplete: (await OnboardingHistories.find(selector).countDocuments()) > 0,
-      });
+        if (actionsMap.includes('manageForms')) {
+          results.push({
+            feature: 'properties',
+            isComplete: true,
+          });
+        }
+
+        continue;
+      }
+
+      const { showModule, showActions } = checkShowModule(actionsMap, feature);
+
+      if (showModule) {
+        const actions = featureActions[feature] || [];
+        const selector = { userId: user._id, completedActions: { $all: actions } };
+
+        results.push({
+          feature,
+          actions,
+          showActions,
+          isComplete: (await OnboardingHistories.find(selector).countDocuments()) > 0,
+        });
+      }
     }
 
     return results;
