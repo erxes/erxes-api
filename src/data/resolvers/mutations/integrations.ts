65411b80280a8ab818f71e66c3a0aa591026e83c
Khangarid Bayarsaikhan <khangarid.b@nmma.co>
2019-10-01 12:42:23 +0800

    fix twitter integration remove

--- src/data/resolvers/mutations/integrations.ts
+++ src/data/resolvers/mutations/integrations.ts
@@ -127,7 +127,7 @@ const integrationMutations = {
     const integration = await Integrations.findOne({ _id });
 
     if (integration) {
-      if (['facebook-messenger', 'facebook-post', 'gmail', 'callpro'].includes(integration.kind || '')) {
+      if (['facebook-messenger', 'facebook-post', 'gmail', 'callpro', 'twitter-dm'].includes(integration.kind || '')) {
         await dataSources.IntegrationsAPI.removeIntegration({ integrationId: _id });
       }
 
