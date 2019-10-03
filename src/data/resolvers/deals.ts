f1c5e879dceed7985843f85f9321ba357fdfd4d2
enkhtuvshin0513 <enhtuwshinn@gmail.com>
2019-10-03 13:50:25 +0800

    change customfields deal product

--- src/data/resolvers/deals.ts
+++ src/data/resolvers/deals.ts
@@ -1,4 +1,14 @@
-import { Companies, Conformities, Customers, Notifications, Pipelines, Products, Stages, Users } from '../../db/models';
+import {
+  Companies,
+  Conformities,
+  Customers,
+  Fields,
+  Notifications,
+  Pipelines,
+  Products,
+  Stages,
+  Users,
+} from '../../db/models';
 import { IDealDocument } from '../../db/models/definitions/deals';
 import { IContext } from '../types';
 import { boardId } from './boardUtils';
@@ -28,7 +38,21 @@ export default {
     const products: any = [];
 
     for (const data of deal.productsData || []) {
-      const product = await Products.findOne({ _id: data.productId });
+      const product = await Products.getProduct({ _id: data.productId });
+
+      const { customFieldsData } = product;
+
+      if (customFieldsData) {
+        const customFields = {};
+
+        const fields = await Fields.find({ contentType: 'product' });
+
+        for (const field of fields) {
+          customFields[field.text || field._id] = customFieldsData[field._id];
+        }
+
+        product.customFieldsData = customFields;
+      }
 
       // Add product object to resulting list
       if (data && product) {
