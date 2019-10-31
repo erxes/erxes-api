5784d58ec493d29b0e8e75cb22848fa744e44fdd
enkhtuvshin0513 <enhtuwshinn@gmail.com>
2019-10-31 13:47:50 +0800

    update

--- src/data/resolvers/conformity.ts
+++ src/data/resolvers/conformity.ts
@@ -3,6 +3,6 @@ import { IConformityDocument } from '../../db/models/definitions/conformities';
 
 export default {
   createdUser(conformity: IConformityDocument) {
-    return Users.findOne({ _id: conformity._id });
+    return Users.findOne({ _id: conformity.createdBy }) || {};
   },
 };
