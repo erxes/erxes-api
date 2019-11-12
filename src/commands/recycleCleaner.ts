cc0c6d1214b74dc5ab061278c2d1d3426bf38d87
munkhsaikhan <munkhsaikhan.g@nmma.co>
2019-11-12 13:42:25 +0800

    continue

--- src/commands/recycleCleaner.ts
+++ src/commands/recycleCleaner.ts
@@ -1,10 +1,15 @@
+import * as dotenv from 'dotenv';
 import { createConnection } from 'mongoose';
 
+dotenv.config();
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+};
+
 const main = async () => {
-  const mongoClient = await createConnection(process.env.MONGO_URL || '', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-  });
+  const mongoClient = await createConnection(process.env.MONGO_URL || '', options);
 
   const recycleCleanerInKey = async coll => {
     const collection = mongoClient.db.collection(coll);
@@ -13,26 +18,29 @@ const main = async () => {
       .aggregate([{ $match: { customFieldsData: { $exists: true } } }, { $project: { _id: 1, customFieldsData: 1 } }])
       .toArray();
 
-    for (const entry of entries) {
+    entries.forEach(async entry => {
       const tempDic = entry.customFieldsData;
       let checkCount = 0;
 
+      // todo fix unwait
       Object.keys(tempDic).forEach(async key => {
         const len = await fields.find({ _id: key }).count();
         if (len === 0) {
           delete tempDic[key];
-          checkCount = +1;
+          checkCount += 1;
         }
       });
 
       if (checkCount > 0) {
         await collection.updateOne({ _id: entry._id }, { $set: { customFieldsData: tempDic } });
       }
-    }
+    });
   };
 
   await recycleCleanerInKey('products');
   await recycleCleanerInKey('companies');
+
+  process.exit();
 };
 
 main();
