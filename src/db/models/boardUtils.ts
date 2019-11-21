f17b791276eb35c030e58a0737d08d8651e33817
munkhjin <munkhjin0223@gmail.com>
2019-11-21 11:10:57 +0800

    update

--- src/db/models/boardUtils.ts
+++ src/db/models/boardUtils.ts
@@ -56,7 +56,7 @@ export const watchItem = async (collection: any, _id: string, isAdd: boolean, us
   return collection.findOne({ _id });
 };
 
-export const fillSearchTextItem = (doc: any, item?: IItemCommonFields) => {
+export const fillSearchTextItem = (doc: IItemCommonFields, item?: IItemCommonFields) => {
   const document = item || { name: '', description: '' };
   Object.assign(document, doc);
 
