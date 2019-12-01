80167ad8743939961eb4c9deb24bd1ef673ed5a2
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-12-01 20:40:07 +0800

    remove doNotDisturb field setter migration

--- src/migrations/17737360261250-add-donotdisturb-field.ts
+++
@@ -1,8 +0,0 @@
-import { connect } from '../db/connection';
-import { Users } from '../db/models';
-
-module.exports.up = async () => {
-  await connect();
-
-  await Users.updateMany({}, { $set: { doNotDisturb: 'No' } });
-};
