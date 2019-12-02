b0b52e2ea424eb70257d872e079ee74786393743
enkhtuvshin0513 <enhtuwshinn@gmail.com>
2019-12-02 13:03:44 +0800

    add migrate

---
+++ src/migrations/18052642328142-activity-log.ts
@@ -0,0 +1,49 @@
+import { connect } from '../db/connection';
+import { ActivityLogs, EngageMessages, InternalNotes } from '../db/models';
+
+/**
+ * Rename createdDate field to createdAt
+ *
+ */
+module.exports.up = async () => {
+  await connect();
+
+  await InternalNotes.updateMany({}, { $rename: { createdDate: 'createdAt' } });
+  await EngageMessages.updateMany({}, { $rename: { createdDate: 'createdAt' } });
+
+  const activities: any = await ActivityLogs.find({ 'activity.action': { $in: ['merge', 'create'] } });
+
+  for (let activity of activities) {
+    activity = activity.toJSON();
+
+    if (activity.activity) {
+      const { action, content, id, type } = activity.activity;
+      const { performedBy } = activity;
+
+      if (action === 'merge') {
+        await ActivityLogs.create({
+          contentId: id,
+          contentType: type,
+          content: content.split(','),
+          action,
+          createdBy: performedBy.id,
+        });
+
+        await ActivityLogs.deleteOne({ _id: activity._id });
+      }
+
+      if (action === 'create' && type !== 'conversation' && type !== 'internal_note') {
+        await ActivityLogs.create({
+          contentId: id,
+          contentType: type,
+          action,
+          createdBy: performedBy.id,
+        });
+
+        await ActivityLogs.deleteOne({ _id: activity._id });
+      }
+    }
+  }
+
+  return ActivityLogs.deleteMany({ performedBy: { $exists: true } });
+};
