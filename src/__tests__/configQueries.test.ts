f8ea59ba64b6bb3b34c3f7cf9af0ca99e918426f
BatAmar Battulga <batamar.b@nmtec.co>
2020-01-12 11:32:21 +0800

    test fix

--- src/__tests__/configQueries.test.ts
+++ src/__tests__/configQueries.test.ts
@@ -1,29 +1,8 @@
 import { graphqlRequest } from '../db/connection';
-import { configFactory } from '../db/factories';
 
 import './setup.ts';
 
 describe('configQueries', () => {
-  test('config detail', async () => {
-    const config = await configFactory();
-
-    const args = { code: config.code };
-
-    const qry = `
-      query configsDetail($code: String!) {
-        configsDetail(code: $code) {
-          _id
-          code
-          value
-        }
-      }
-    `;
-
-    const response = await graphqlRequest(qry, 'configsDetail', args);
-
-    expect(response.code).toBe(config.code);
-  });
-
   test('config get env', async () => {
     process.env.USE_BRAND_RESTRICTIONS = 'true';
 
