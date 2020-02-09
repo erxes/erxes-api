d1143479efd7f9c8bacaa6f8737eddd84549371d
BatAmar Battulga <batamar.b@nmtec.co>
2020-02-09 19:20:25 +0800

    fieldQuery tests

--- src/__tests__/fieldQueries.test.ts
+++ src/__tests__/fieldQueries.test.ts
@@ -83,8 +83,8 @@ describe('fieldQueries', () => {
     await fieldFactory({ contentType: 'customer', groupId: invisibleGroup._id });
 
     const qry = `
-      query fieldsCombinedByContentType($contentType: String!, $source: String) {
-        fieldsCombinedByContentType(contentType: $contentType, source: $source)
+      query fieldsCombinedByContentType($contentType: String!) {
+        fieldsCombinedByContentType(contentType: $contentType)
       }
     `;
 
@@ -121,14 +121,7 @@ describe('fieldQueries', () => {
 
     responses = await graphqlRequest(qry, 'fieldsCombinedByContentType', {
       contentType: 'customer',
-      source: 'fromSegments',
     });
-
-    const data1 = responses.find(response => response.name === 'messengerData.customData.data1');
-    const data2 = responses.find(response => response.name === 'messengerData.customData.data2');
-
-    expect(data1).toBeDefined();
-    expect(data2).toBeDefined();
 
     responses = await graphqlRequest(qry, 'fieldsCombinedByContentType', { contentType: 'customer' });
 
