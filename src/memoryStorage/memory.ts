404a0837532ee0032172707d449d84f96b156a7e
BatAmar Battulga <batamar.b@nmtec.co>
2020-08-05 15:38:49 +0800

    removed console.log

--- src/memoryStorage/memory.ts
+++ src/memoryStorage/memory.ts
@@ -7,8 +7,6 @@ export const get = (key: string, defaultValue?: any): Promise<any> => {
   return new Promise(resolve => {
     const value = storage[key];
 
-    console.log('in get ........... ');
-
     return resolve(value || defaultValue);
   });
 };
@@ -17,8 +15,6 @@ export const get = (key: string, defaultValue?: any): Promise<any> => {
  * Set item
  */
 export const set = (key: string, value: any) => {
-  console.log('in set ........... ');
-
   storage[key] = value;
 };
 
@@ -31,7 +27,6 @@ export const inArray = async (setKey: string, setMember: string): Promise<any> =
   if (!value) {
     return false;
   }
-  console.log('in array ........... ');
 
   return value.includes(setMember);
 };
@@ -40,8 +35,6 @@ export const inArray = async (setKey: string, setMember: string): Promise<any> =
  * Add a value to a set or do nothing if it already exists
  */
 export const addToArray = (setKey: string, setMember: string) => {
-  console.log('add to array ........... ');
-
   const value = storage[setKey];
 
   if (value) {
@@ -53,8 +46,6 @@ export const addToArray = (setKey: string, setMember: string) => {
  * Remove a value from a set or do nothing if it is not present
  */
 export const removeFromArray = (setKey: string, setMember: string) => {
-  console.log('remove from array ........... ');
-
   const value = storage[setKey];
 
   if (value) {
