e3ad040c0e5abccf64861d7ed5933fa8a896c6e5
Munkh-Orgil <munkhorgil@live.com>
2020-08-13 19:40:24 +0800

    update erxes-inmemory-storage

--- src/inmemoryStorage.ts
+++ src/inmemoryStorage.ts
@@ -1,10 +1,13 @@
 import * as dotenv from 'dotenv';
 import * as memoryStorage from 'erxes-inmemory-storage';
+import { debugBase } from './debuggers';
 
 // load environment variables
 dotenv.config();
 
 const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
+
+debugBase(`Memory storage using: ${REDIS_HOST ? 'Redis' : 'Local'}`);
 
 memoryStorage.init({
   host: REDIS_HOST,
